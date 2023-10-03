2a46846a7fca0c58e3e28f51b30d705c98430aef
WithinKael <138517664+WithinKael@users.noreply.github.com>
2023-10-03 22:11:05 +0200

    update react

---
+++ src/components/ContactsForm.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  onInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onBtnSubmit = event => {
+    event.preventDefault();
+    const { name, number } = this.state;
+
+    const isDuplicateName = this.props.contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (isDuplicateName) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    if (name === '') {
+      alert('Enter your name');
+      return;
+    }
+
+    const newContact = { id: nanoid(), name, number };
+    this.props.onAddContact(newContact);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form onSubmit={this.onBtnSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={this.onInputChange}
+        />
+        <label>Number</label>
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={this.onInputChange}
+        />
+        <button type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
